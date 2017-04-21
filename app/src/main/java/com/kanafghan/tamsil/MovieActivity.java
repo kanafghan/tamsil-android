99ec46a9cc40f2c15de640204ab0a72f5d84c46d
Ismail Faizi <kanafghan@gmail.com>
2017-04-21 21:52:00 +0530

    add menu with Settings item to the movie detials activity

--- app/src/main/java/com/kanafghan/tamsil/MovieActivity.java
+++ app/src/main/java/com/kanafghan/tamsil/MovieActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MovieActivity extends AppCompatActivity {
@@ -24,12 +25,24 @@ public class MovieActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.movie, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if (id == android.R.id.home) {
             Intent mainIntent = new Intent(this, MainActivity.class);
             startActivity(mainIntent);
+            return true;
+        }
+
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
             return true;
         }
 
